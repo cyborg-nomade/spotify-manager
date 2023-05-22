f525a99afaa59698953e1a9f56fb50db54d01a1a
Uriel Fiori <u.fiori@iib-institut.de>
2023-05-22 15:24:59 +0200

    extract initial data done

--- spotify_manager/convert_control_file.py
+++ spotify_manager/convert_control_file.py
@@ -6,11 +6,17 @@ from spotify_manager.models.file_items import ControlFileItem
 def extract_initial_data(file_path: str) -> list[ControlFileItem]:
     """Extract data in the initial control file and return a list of ControlFileItem."""
 
+    split_line_parts = []
+
     with open(file_path, "r") as initial_control_file:
         lines = initial_control_file.readlines()
-        print(lines)
+        for index, line in enumerate(lines):
+            line_parts = line.split("|")
+            if len(line_parts) != 5:
+                print(index)
+            split_line_parts.append(line_parts)
 
-    return []
+    return split_line_parts
 
 
 if __name__ == "__main__":
