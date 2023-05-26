489748c1de468468f8b5a4d88506810486b6bba8
Uriel Fiori <u.fiori@iib-institut.de>
2023-05-26 13:30:43 +0200

    created monthly routine code

---
+++ spotify_manager/routines/monthly_routine.py
@@ -0,0 +1,23 @@
+"""Execute all monthly routines."""
+
+from spotify_manager.loaders_savers import load_control_file
+from spotify_manager.processors.control_file_processors import (
+    check_album_results,
+    get_starting_index,
+)
+from spotify_manager.processors.stats_processors import update_stats
+from spotify_manager.processors.total_albums_processor import (
+    add_monthly_albums,
+    update_total_album_list,
+)
+
+
+def run_monthly_routines() -> None:
+    """Run all monthly routines."""
+    control_file = load_control_file()
+    total_album_list = update_total_album_list()
+
+    check_album_results(control_file)
+    update_stats(control_file, total_album_list)
+    starting_index = get_starting_index(control_file, total_album_list)
+    add_monthly_albums(control_file, total_album_list, starting_index)
