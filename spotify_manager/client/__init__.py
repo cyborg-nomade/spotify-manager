93341114798501da8fa694f738c769b9f59eb2cd
Uriel Fiori <u.fiori@iib-institut.de>
2023-06-01 09:57:47 +0200

    refactored client to remove secrets from code

--- spotify_manager/client/__init__.py
+++ spotify_manager/client/__init__.py
@@ -3,12 +3,18 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+# UFI
+from spotify_manager.settings import Settings
+
+
+settings = Settings()
+
 
 def get_spotipy_client() -> spotipy.Spotify:
-    """."""
-    spotipy_client_id = "fc70707ebf5d4ca3af5bdcd88bdd9b17"
-    spotipy_client_secret = "bc624df47d1c48b5a3f5dcef186c2b6c"
-    spotipy_redirect_uri = "http://localhost"
+    """Get spotipy client."""
+    spotipy_client_id = settings.spotipy_client_id
+    spotipy_client_secret = settings.spotipy_client_secret
+    spotipy_redirect_uri = settings.spotipy_redirect_uri
 
     scope = ["playlist-modify-public", "playlist-modify-private", "user-library-read"]
 
@@ -18,5 +24,5 @@ def get_spotipy_client() -> spotipy.Spotify:
             client_id=spotipy_client_id,
             client_secret=spotipy_client_secret,
             redirect_uri=spotipy_redirect_uri,
-        )
+        ),
     )
